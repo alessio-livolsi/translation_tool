81531685c5b8c77335f8ab571c04a08586bd7b29
Alessio <alessio.livolsi@protonmail.com>
2023-03-20 11:51:25 +0000

    detect lang code and add it to the filename

--- script.py
+++ script.py
@@ -16,7 +16,7 @@ logging.basicConfig(
 )
 
 # Load the PO file
-po_file = polib.pofile("django.po")
+po_file = polib.pofile("your load filename")
 
 # Translate each message in the PO file
 translator = Translator()
@@ -28,7 +28,7 @@ for entry in tqdm(po_file, desc="Translating messages"):
     # dest = language code
     # All language codes can be found here > https://py-googletrans.readthedocs.io/en/latest/#googletrans-languages
     try:
-        translation = translator.translate(entry.msgid, dest="es").text
+        translation = translator.translate(entry.msgid, dest="your required language code").text
     except Exception as e:
         logging.error(f"Error translating '{entry.msgid}': {e}")
         continue
@@ -36,7 +36,8 @@ for entry in tqdm(po_file, desc="Translating messages"):
     entry.msgstr = translation
 
 # Save the translated PO file with a new filename
-base_filename = os.path.splitext("django.po")[0]
-translated_filename = f"{base_filename}_es.po"
+base_filename = os.path.splitext("your load filename")[0]
+dest_lang_code = "enter the dest"
+translated_filename = f"{base_filename}_{dest_lang_code}.po"
 po_file.save(translated_filename)
 print(f"Your file has been translated and saved as {translated_filename}.")
